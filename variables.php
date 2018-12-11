ec4136b06f5922035e96de6ea4ace11252f02593
sanjaypj <sanjaypjayan2000@gmail.com>
2018-12-11 21:42:14 +0530

    init2

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php 
+	echo "hello world!\n";
+	print 'hello world';
+	//this is a comment
+	#also can use this one
+	$output = 'Hello world';
+	print $output;
+	print "\n$output\n";
+	echo $output . $output . "\n";
+	print "$output $output\n";
+	define('GREETING', 'Hellow World!!!!');
+	echo GREETING;
+?>
